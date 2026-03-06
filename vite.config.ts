cd83412d4b1ba73b521cf04b3e8d1ccc40703193
thanonhari <thanonhari@gmail.com>
2026-03-06 22:24:39 +0700

    Deployment: Configure for Cloudflare Pages (Root Base)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/it-services/', // ต้องระบุชื่อ Repository เพื่อให้ GitHub Pages โหลดไฟล์ถูกครับ
+  // Cloudflare Pages ปกติจะใช้ root path (/) เป็นหลักครับ
+  base: '/', 
 })
