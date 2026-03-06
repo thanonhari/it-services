fd60ed149d618f5d0f66fea8d970ccbb02cca4d4
thanonhari <thanonhari@gmail.com>
2026-03-06 22:17:26 +0700

    Automation: Setup GitHub Actions for Pages Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/it-services/', // ต้องระบุชื่อ Repository เพื่อให้ GitHub Pages โหลดไฟล์ถูกครับ
 })
